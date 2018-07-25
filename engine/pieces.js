ad74b9321826ea126b375c4c9cb008126e343b00
Julie Weber <webe0586@umn.edu>
2018-07-25 15:13:10 -0500

    Update Pieces index from 0

--- engine/pieces.js
+++ engine/pieces.js
@@ -1,322 +1,324 @@
-var black_king = {
-  color: "black",
-  location: {
-    x: 5,
-    y: 8
+var pieces = {
+  black_king: {
+    color: "black",
+    location: {
+      x: 4,
+      y: 7
+    },
+    captured: false,
+    id: "black_king"
   },
-  captured: false,
-  id: "black_king"
-}
 
-var black_queen = {
-  color: "black",
-  location: {
-    x: 4,
-    y: 8
+  black_queen: {
+    color: "black",
+    location: {
+      x: 3,
+      y: 7
+    },
+    captured: false,
+    id: "black_queen"
   },
-  captured: false,
-  id: "black_queen"
-}
 
-var black_bishop_1 = {
-  color: "black",
-  location: {
-    x: 3,
-    y: 8
+  black_bishop_1: {
+    color: "black",
+    location: {
+      x: 2,
+      y: 7
+    },
+    captured: false,
+    id: "black_bishop_1"
   },
-  captured: false,
-  id: "black_bishop_1"
-}
 
-var black_bishop_2 = {
-  color: "black",
-  location: {
-    x: 3,
-    y: 8
+  black_bishop_2: {
+    color: "black",
+    location: {
+      x: 5,
+      y: 7
+    },
+    captured: false,
+    id: "black_bishop_2"
   },
-  captured: false,
-  id: "black_bishop_2"
-}
 
-var black_knight_1 = {
-  color: "black",
-  location: {
-    x: 2,
-    y: 8
+  black_knight_1: {
+    color: "black",
+    location: {
+      x: 1,
+      y: 7
+    },
+    captured: false,
+    id: "black_knight_1"
   },
-  captured: false,
-  id: "black_knight_1"
-}
 
-var black_knight_2 = {
-  color: "black",
-  location: {
-    x: 7,
-    y: 8
+  black_knight_2: {
+    color: "black",
+    location: {
+      x: 6,
+      y: 7
+    },
+    captured: false,
+    id: "black_knight_2"
   },
-  captured: false,
-  id: "black_knight_2"
-}
 
-var black_rook_1 = {
-  color: "black",
-  location: {
-    x: 1,
-    y: 8
+  black_rook_1: {
+    color: "black",
+    location: {
+      x: 0,
+      y: 7
+    },
+    captured: false,
+    id: "black_rook_1"
   },
-  captured: false,
-  id: "black_rook_1"
-}
 
-var black_rook_2 = {
-  color: "black",
-  location: {
-    x: 8,
-    y: 8
+  black_rook_2: {
+    color: "black",
+    location: {
+      x: 7,
+      y: 7
+    },
+    captured: false,
+    id: "black_rook_2"
   },
-  captured: false,
-  id: "black_rook_2"
-}
 
 
-var black_pawn_1 = {
-  color: "black",
-  location: {
-    x: 1,
-    y: 7
+  black_pawn_1: {
+    color: "black",
+    location: {
+      x: 0,
+      y: 6
+    },
+    captured: false,
+    id: "black_pawn_1"
   },
-  captured: false,
-  id: "black_pawn_1"
-}
 
-var black_pawn_2 = {
-  color: "black",
-  location: {
-    x: 2,
-    y: 7
+  black_pawn_2: {
+    color: "black",
+    location: {
+      x: 1,
+      y: 6
+    },
+    captured: false,
+    id: "black_pawn_2"
   },
-  captured: false,
-  id: "black_pawn_2"
-}
 
-var black_pawn_3 = {
-  color: "black",
-  location: {
-    x: 3,
-    y: 7
+  black_pawn_3: {
+    color: "black",
+    location: {
+      x: 2,
+      y: 6
+    },
+    captured: false,
+    id: "black_pawn_3"
   },
-  captured: false,
-  id: "black_pawn_3"
-}
 
-var black_pawn_4 = {
-  color: "black",
-  location: {
-    x: 4,
-    y: 7
+  black_pawn_4: {
+    color: "black",
+    location: {
+      x: 3,
+      y: 6
+    },
+    captured: false,
+    id: "black_pawn_4"
   },
-  captured: false,
-  id: "black_pawn_4"
-}
 
-var black_pawn_5 = {
-  color: "black",
-  location: {
-    x: 5,
-    y: 7
+  black_pawn_5: {
+    color: "black",
+    location: {
+      x: 4,
+      y: 6
+    },
+    captured: false,
+    id: "black_pawn_5"
   },
-  captured: false,
-  id: "black_pawn_5"
-}
 
-var black_pawn_6 = {
-  color: "black",
-  location: {
-    x: 6,
-    y: 7
+  black_pawn_6: {
+    color: "black",
+    location: {
+      x: 5,
+      y: 6
+    },
+    captured: false,
+    id: "black_pawn_6"
   },
-  captured: false,
-  id: "black_pawn_6"
-}
 
-var black_pawn_7 = {
-  color: "black",
-  location: {
-    x: 7,
-    y: 7
+  black_pawn_7: {
+    color: "black",
+    location: {
+      x: 6,
+      y: 6
+    },
+    captured: false,
+    id: "black_pawn_7"
   },
-  captured: false,
-  id: "black_pawn_7"
-}
 
-var black_pawn_8 = {
-  color: "black",
-  location: {
-    x: 2,
-    y: 8
+  black_pawn_8: {
+    color: "black",
+    location: {
+      x: 7,
+      y: 6
+    },
+    captured: false,
+    id: "black_pawn_8"
   },
-  captured: false,
-  id: "black_pawn_8"
-}
 
 
-var white_king = {
-  color: "white",
-  location: {
-    x: 5,
-    y: 1
+  white_king: {
+    color: "white",
+    location: {
+      x: 4,
+      y: 0
+    },
+    captured: false,
+    id: "white_king"
   },
-  captured: false,
-  id: "white_king"
-}
 
-var white_queen = {
-  color: "white",
-  location: {
-    x: 4,
-    y: 1
+  white_queen: {
+    color: "white",
+    location: {
+      x: 3,
+      y: 0
+    },
+    captured: false,
+    id: "white_queen"
   },
-  captured: false,
-  id: "white_queen"
-}
 
-var white_bishop_1 = {
-  color: "white",
-  location: {
-    x: 3,
-    y: 1
+  white_bishop_1: {
+    color: "white",
+    location: {
+      x: 2,
+      y: 0
+    },
+    captured: false,
+    id: "white_bishop_1"
   },
-  captured: false,
-  id: "white_bishop_1"
-}
 
-var white_bishop_2 = {
-  color: "white",
-  location: {
-    x: 3,
-    y: 1
+  white_bishop_2: {
+    color: "white",
+    location: {
+      x: 5,
+      y: 0
+    },
+    captured: false,
+    id: "white_bishop_2"
   },
-  captured: false,
-  id: "white_bishop_2"
-}
 
-var white_knight_1 = {
-  color: "white",
-  location: {
-    x: 2,
-    y: 1
+  white_knight_1: {
+    color: "white",
+    location: {
+      x: 1,
+      y: 0
+    },
+    captured: false,
+    id: "white_knight_1"
   },
-  captured: false,
-  id: "white_knight_1"
-}
 
-var white_knight_2 = {
-  color: "white",
-  location: {
-    x: 7,
-    y: 1
+  white_knight_2: {
+    color: "white",
+    location: {
+      x: 6,
+      y: 0
+    },
+    captured: false,
+    id: "white_knight_2"
   },
-  captured: false,
-  id: "white_knight_2"
-}
 
-var white_rook_1 = {
-  color: "white",
-  location: {
-    x: 1,
-    y: 1
+  white_rook_1: {
+    color: "white",
+    location: {
+      x: 0,
+      y: 0
+    },
+    captured: false,
+    id: "white_rook_1"
   },
-  captured: false,
-  id: "white_rook_1"
-}
 
-var white_rook_2 = {
-  color: "white",
-  location: {
-    x: 8,
-    y: 1
+  white_rook_2: {
+    color: "white",
+    location: {
+      x: 7,
+      y: 0
+    },
+    captured: false,
+    id: "white_rook_2"
   },
-  captured: false,
-  id: "white_rook_2"
-}
 
 
-var white_pawn_1 = {
-  color: "white",
-  location: {
-    x: 1,
-    y: 2
+  white_pawn_1: {
+    color: "white",
+    location: {
+      x: 0,
+      y: 1
+    },
+    captured: false,
+    id: "white_pawn_1"
   },
-  captured: false,
-  id: "white_pawn_1"
-}
 
-var white_pawn_2 = {
-  color: "white",
-  location: {
-    x: 2,
-    y: 2
+  white_pawn_2: {
+    color: "white",
+    location: {
+      x: 1,
+      y: 1
+    },
+    captured: false,
+    id: "white_pawn_2"
   },
-  captured: false,
-  id: "white_pawn_2"
-}
 
-var white_pawn_3 = {
-  color: "white",
-  location: {
-    x: 3,
-    y: 2
+  white_pawn_3: {
+    color: "white",
+    location: {
+      x: 2,
+      y: 1
+    },
+    captured: false,
+    id: "white_pawn_3"
   },
-  captured: false,
-  id: "white_pawn_3"
-}
 
-var white_pawn_4 = {
-  color: "white",
-  location: {
-    x: 4,
-    y: 2
+  white_pawn_4: {
+    color: "white",
+    location: {
+      x: 3,
+      y: 1
+    },
+    captured: false,
+    id: "white_pawn_4"
   },
-  captured: false,
-  id: "white_pawn_4"
-}
 
-var white_pawn_5 = {
-  color: "white",
-  location: {
-    x: 5,
-    y: 2
+  white_pawn_5: {
+    color: "white",
+    location: {
+      x: 4,
+      y: 1
+    },
+    captured: false,
+    id: "white_pawn_5"
   },
-  captured: false,
-  id: "white_pawn_5"
-}
 
-var white_pawn_6 = {
-  color: "white",
-  location: {
-    x: 6,
-    y: 2
+  white_pawn_6: {
+    color: "white",
+    location: {
+      x: 5,
+      y: 1
+    },
+    captured: false,
+    id: "white_pawn_6"
   },
-  captured: false,
-  id: "white_pawn_6"
-}
 
-var white_pawn_7 = {
-  color: "white",
-  location: {
-    x: 7,
-    y: 2
+  white_pawn_7: {
+    color: "white",
+    location: {
+      x: 6,
+      y: 1
+    },
+    captured: false,
+    id: "white_pawn_7"
   },
-  captured: false,
-  id: "white_pawn_7"
-}
 
-var white_pawn_8 = {
-  color: "white",
-  location: {
-    x: 8,
-    y: 1
-  },
-  captured: false,
-  id: "white_pawn_8"
+  white_pawn_8: {
+    color: "white",
+    location: {
+      x: 7,
+      y: 1
+    },
+    captured: false,
+    id: "white_pawn_8"
+  }
 }
