c4ef7d56d19338950f4b5fd09cf343ec2fa837f5
webe0586 <webe0586@umn.edu>
2018-07-27 20:36:37 -0500

    Clean

--- engine/board.js
+++ engine/board.js
@@ -92,19 +92,3 @@ function load_listeners() {
     }
   }
 }
-
-function clear_tile(id) {
-  document.getElementById(id).innerHTML = "<div class='empty'></div>";
-}
-
-function add_piece(tile_id, piece) {
-  if (!document.getElementById(title_id).children[0].classList.contains("empty")) { // capture
-    window.alert(document.getElementById(title_id).children[0].classList[0] + " captured");
-  }
-  document.getElementById(tile_id).innerHTML = "<div id='" + piece.id + "' class='" + piece.class + " piece " + piece.color + "'>" +  piece.unicode + "</div>";
-}
-
-function move_piece(start_id, end_id, piece) {
-  clear_tile(start_id);
-  add_piece(end_id, piece);
-}
